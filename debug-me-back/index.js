421e927501105911d95e8837f7a7cba027671e5e
Ben Paget <ben.paget@live.fr>
2020-11-08 12:46:00 +0000

    Debugged Front

--- debug-me-back/index.js
+++ debug-me-back/index.js
@@ -3,7 +3,7 @@ const upload = require('./utils/imageUploader')
 const path = require('path')
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname, '/public')));
 
--- debug-me-front/src/App.js
+++ debug-me-front/src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useCallback } from "react";
+import Grid from "@material-ui/core/Grid";
+import { useDropzone } from "react-dropzone";
+import CardDisplay from "./CardDisplay";
 
-function App() {
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const activeStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+export default function App() {
+  const [pathToPic, setPathToPic] = useState(null);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const formData = new FormData();
+    formData.append("profile_pic", acceptedFiles[0]);
+
+    fetch("/upload-profile-pic", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then(({pathToImage}) => {
+          if (pathToImage) setPathToPic(pathToImage)
+          else console.log('Image upload failed')
+        })
+      .catch((e) => console.log(e.message));
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ accept: "image/*", multiple: false, onDrop });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      style={{ height: "100vh" }}
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      {!pathToPic ? (
+        <div className="container">
+          <div {...getRootProps({ style })}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' a file here, or click to select file</p>
+          </div>
+        </div>
+      ) : (
+        <CardDisplay pic={pathToPic} />
+      )}
+    </Grid>
   );
 }
-
-export default App;
