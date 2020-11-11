b80067ed2a6febc26b453d94cbbc244d2501512d
Ben Paget <ben.paget@live.fr>
2020-11-11 22:52:13 +0000

    Destroyed the front-end

--- debug-me-front/src/App.js
+++ debug-me-front/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useMemo, useCallback } from "react";
 import './App.css'
 import Grid from "@material-ui/core/Grid";
-import { useDropzone } from "react-dropzone";
+import useDropzone from "react-dropzone";
 import CardDisplay from "./CardDisplay";
 
 const baseStyle = {
@@ -33,19 +33,18 @@ const rejectStyle = {
 };
 
 export default function App() {
-  const [pathToPic, setPathToPic] = useState(null);
+  const {pathToPic, setPathToPic} = useState(null);
 
   const onDrop = useCallback((acceptedFiles) => {
     const formData = new FormData();
     formData.append("profile_pic", acceptedFiles[0]);
 
-    fetch("/upload-profile-pic", {
+    fetch("/upload-pic", {
       method: "POST",
       body: formData,
     })
-      .then((res) => res.json())
       .then(({pathToImage}) => {
-          if (pathToImage) setPathToPic(pathToImage)
+          if (!pathToImage) setPathToPic(pathToImage)
           else console.log('Image upload failed')
         })
       .catch((e) => console.log(e.message));
@@ -60,7 +59,7 @@ export default function App() {
   } = useDropzone({ accept: "image/*", multiple: false, onDrop });
 
   const style = useMemo(
-    () => ({
+   ({
       ...baseStyle,
       ...(isDragActive ? activeStyle : {}),
       ...(isDragAccept ? acceptStyle : {}),
@@ -77,7 +76,7 @@ export default function App() {
       justify="center"
       alignItems="center"
     >
-      {!pathToPic ? (
+      {!pathToPic && (
         <div className="container">
         <h1 className="title">[DEBUG ME GENTLY]</h1>
         <img src="https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Programmer-while-sleeping.jpg" className="pic" alt="bug" />
--- debug-me-front/src/CardDisplay.js
+++ debug-me-front/src/CardDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,6 +7,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Alert from '@material-ui/core/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
 
 const useStyles = makeStyles({
   root: {
@@ -17,8 +19,10 @@ const useStyles = makeStyles({
   },
 });
 
-export default function CardDisplay({pic}) {
+const CardDisplay = (pic) => {
   const classes = useStyles();
+
+  const [open, setOpen] = useState(true)
 
   return (
     <Card className={classes.root}>
@@ -46,5 +50,12 @@ export default function CardDisplay({pic}) {
         </Button>
       </CardActions>
     </Card>
+    <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+  <Alert onClose={() => setOpen(false)} severity="success">
+    Profile picture uploaded successfully!
+  </Alert>
+</Snackbar>
   );
 }
+
+export default CardDisplay
